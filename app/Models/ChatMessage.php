dd3dd7e7370b1e54950d636371aa705516574b62
hossein <rahbartalab2002@gmail.com>
2023-09-02 20:41:45 +0330

    [feat] create models and relations between those

---
+++ app/Models/ChatMessage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChatMessage extends Model
+{
+    use HasFactory;
+
+    public function room(): \Illuminate\Database\Eloquent\Relations\hasOne
+    {
+        return $this->hasOne(ChatRoom::class, 'id', 'chat_room_id');
+    }
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+}
