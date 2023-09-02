df0f14025f01e15650bd51b7341401285b84c0a3
hossein <rahbartalab2002@gmail.com>
2023-09-02 20:42:28 +0330

    [feat] create controller and methods v.1

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChatMessage;
+use App\Models\ChatRoom;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ChatController extends Controller
+{
+    public function rooms(Request $request): \Illuminate\Database\Eloquent\Collection
+    {
+        return ChatRoom::all();
+    }
+
+    public function messages(Request $request, $roomId): \Illuminate\Database\Eloquent\Collection|array
+    {
+        return ChatMessage::query()->where('chat_room_id', $roomId)
+            ->with('user')
+            ->orderBy('created_at', 'DESC')
+            ->get();
+    }
+
+    public function createMessage(Request $request, $roomId): void
+    {
+        ChatMessage::query()->create([
+            'message' => $request->get('message'),
+            'user_id' => Auth::id(),
+            'chat_room_id' => $roomId
+        ]);
+    }
+}
